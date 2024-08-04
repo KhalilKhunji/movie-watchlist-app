9f0d8517e8eeeb533d2afd7c49fc9a17a85fc2b7
KhalilKhunji <khalilkhunji@gmail.com>
2024-08-04 12:33:01 +0300

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import './App.css';
+
 const App = () => {
+  const movies = [
+    { name: 'The Bourne Ultimatum', watched: true },
+    { name: 'Metropolis', watched: false },
+    { name: 'There Will Be Blood', watched: true },
+  ];
 
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Movies to Watch</h1>
+      <ul>
+        {movies.map((movie, index) => (
+          <li key={index} className={movie.watched ? 'watched' : 'not-watched'}>
+            {movie.name}
+          </li>
+        ))}
+      </ul>
+    </>
   );
-}
+};
 
-export default App
+export default App;
